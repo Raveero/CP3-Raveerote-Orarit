b5cddf4d7d73b2f5f225a51fb589ec988a969d29
Raveero <92707912+Raveero@users.noreply.github.com>
2024-04-18 00:16:01 +0700

    ex4

---
+++ Exercise4_Raveerote_O.py
@@ -0,0 +1,9 @@
+GBU = float(input("Enter General Business Score:"))
+ICS = float(input("Enter Introduction to Computer Systems Score"))
+CP = float(input("Enter Computer Programming Score:"))
+
+print("---Your Score---")
+print("Foundation English:", FE)
+print("General Business:", GBU)
+print("Introduction to computer systems:", ICS)
+print("Computer programming:", CP)
