a5583d8ea29733627e823cbf36695865efd9b06b
Raveero <92707912+Raveero@users.noreply.github.com>
2024-05-01 14:12:43 +0700

    add lec50

---
+++ lec50.py
@@ -0,0 +1,23 @@
+x = int(input())
+y = int(input())
+
+def addNumber(x,y):
+    print(x+y)
+
+
+def multiply(x,y):
+    print(x*y)
+
+
+def subtract(x,y):
+    print(x-y)
+
+
+def divide(x,y):
+    print(x/y)
+
+
+addNumber(x,y)
+multiply(x,y)
+subtract(x,y)
+divide(x,y)
